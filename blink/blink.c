50a9e03ed86837c8afedb6841ca9f400d7c53112
lkayo14 <luizkayo11@gmail.com>
2024-12-27 13:57:08 -0300

    nova atualização 27/12

--- blink/blink.c
+++ blink/blink.c
@@ -4,50 +4,30 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
 
-// Pico W devices use a GPIO on the WIFI chip for the LED,
-// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h"
-#endif
-
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
-#endif
-
-// Perform initialisation
-int pico_led_init(void) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-    // so we can use normal GPIO functionality to turn the led on and off
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // For Pico W devices we need to initialise the driver etc
-    return cyw43_arch_init();
-#endif
-}
-
-// Turn the led on or off
-void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // Ask the wifi "driver" to set the GPIO on or off
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-#endif
-}
+const uint led_pin_red = 13;
+const uint led_pin_blue = 12;
 
 int main() {
-    int rc = pico_led_init();
-    hard_assert(rc == PICO_OK);
+    uint a = 100;
+    
+    gpio_init(led_pin_red);
+    gpio_init(led_pin_blue);
+    gpio_set_dir(led_pin_red, GPIO_OUT);
+    gpio_set_dir(led_pin_blue, GPIO_OUT);
+    stdio_init_all();
     while (true) {
-        pico_set_led(true);
-        sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
-        sleep_ms(LED_DELAY_MS);
+        a++;
+        if ( a % 2)
+          printf("Blinking!\r\n");
+        gpio_put(led_pin_red, true);
+        gpio_put(led_pin_blue, true);
+        sleep_ms(1000);
+        gpio_put(led_pin_red, false);
+        gpio_put(led_pin_blue, false);
+        sleep_ms(1000);
+
     }
 }
